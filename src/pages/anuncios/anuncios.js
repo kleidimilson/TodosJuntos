3f761663f002e098f63f82ad8cdf9e6ac84376f1
kleidimilson <kleidimilson@gmail.com>
2020-07-16 19:06:20 -0300

    api

--- src/pages/anuncios/anuncios.js
+++ src/pages/anuncios/anuncios.js
@@ -3,9 +3,10 @@ import Navbar from '../../components/Navbar/Navbar';
 import './anuncios.css';
 import Menu from '../../components/menu/Menu';
 import {FaWhatsapp} from 'react-icons/fa';
-import {RiInformationLine} from 'react-icons/ri';
+import {MdLocationOn} from 'react-icons/md';
 
-
+import {IoMdTime} from 'react-icons/io';
+import {AiFillStar} from 'react-icons/ai';
 import api from '../../services/api';
 const Anuncios = (props) => {
     const [itens, setItens ] = useState([]);
@@ -50,12 +51,12 @@ const Anuncios = (props) => {
                         
                             <div className="cards-description">
                                 <h2>{item.nome}</h2>
-                                <p id="cidade">{item.cidade}</p>
-                                <p>{item.contato}</p>
-                                <p>{item.endereço}</p>
+                                <p id="cidade"><MdLocationOn/>{item.cidade}</p>
+                                <p><AiFillStar color="#c4bc21"/><AiFillStar color="#c4bc21"/><AiFillStar color="#c4bc21"/><AiFillStar color="#c4bc21"/><AiFillStar color="#c4bc21"/><AiFillStar color="#c4bc21"/></p>
+                                <p><IoMdTime/> Segunda a Domingo</p>
                                 <div className="links">
                                      <a id="btn-wpp" href={`https://api.whatsapp.com/send?phone=${item.contato}&text=Olá${item.nome}%20vim%20pelo%20todos%20juntos`}>WhatsApp<FaWhatsapp id="icon-wpp" size={12}/></a>
-                                     <a id="btn-info" href="https://todosjuntos.netlify.app/anuncios/lanches">Ler mais<RiInformationLine id="icon-wpp" size={12}/></a>
+                                    
                                 </div>
                                 
                                 
